47e3e2c89982a61ffad22070758489fb92660a3d
kvandesteeg <2kwv01@gmail.com>
2016-03-05 22:24:40 -0600

    searches for mp3s.  renders ahead of request

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var fs = require('fs');
-var jade = require('jade');
+var fs = require('q-io/fs');
+var pug = require('pug');
 var id3 = require('./id3.js');
-var  Q  = require('q');
+var Q = require('q');
+var compression = require('compression');
 
 var program = require('commander');
 var port, data, host, publicport, image;
@@ -15,22 +16,22 @@ program
   .option('-z, --description [value]', 'public port')
   .option('-i, --image [value]', 'icon for feed')
 
-  .parse(process.argv);
+.parse(process.argv);
 
 
-var rssTemplate = fs.readFileSync('./rss.jade').toString();
-var express    = require('express');    // call express
-var app        = express();         // define our app using express
+var express = require('express'); // call express
+var app = express(); // define our app using express
 //var publicport = 32191;
-var router = express.Router();        // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 //"yellowtail.asuscomm.com"
-var basepath =  program.data || "media";
-var server = program.host+":"+program.publicport;
+var basepath = program.data || "media";
+var feed;
+var server = program.host + ":" + program.publicport;
 var options = {
   feed: {
     title: program.title,
     description: program.description,
-    link: 'http://'+server+"/rss",
+    link: 'http://' + server + "/rss",
     language: 'en-us',
     image: program.image
   },
@@ -42,59 +43,88 @@ var options = {
   }]
 };
 
-function createPost(file){
+function createPost(file) {
   var deferred = Q.defer();
-  id3({ file: basepath+'/'+file, type: id3.OPEN_LOCAL }, function(err, tags) {
-    var newFile= {
+  id3({
+    file: basepath + '/' + file,
+    type: id3.OPEN_LOCAL
+  }, function(err, tags) {
+    var newFile = {
       title: file,
       description: 'post1 summary',
-      canonicalUrl: 'http://'+server+'/media'+encodeURIComponent(file),
+      canonicalUrl: 'http://' + server + '/media' + encodeURIComponent(file),
       pubDate: (new Date()).toGMTString(),
       length: 1
     };
-    if (!err){
-      newFile.description  = tags.year + " " + tags.title;
-      newFile.length= tags.size || 1
+    if (!err) {
+      newFile.description = tags.year + " " + tags.title;
+      newFile.length = tags.size || 1
     } else {
-       newFile.description  = "";
+      newFile.description = "";
     }
     deferred.resolve(newFile)
   });
   return deferred.promise;
 }
 
-function preparePosts(files){
-  var posts=[];
-  files.forEach(function(file){
+function preparePosts(files) {
+  var posts = [];
+  files.forEach(function(file) {
     posts.push(createPost(file));
   });
-  return  Q.all(posts);
+  return Q.all(posts);
 }
-function render(options){
- var deferred = Q.defer();
- jade.render(rssTemplate, options, function (err, xml) {
 
-  if (err) {
-    throw err;
-  }
-  deferred.resolve(xml);
-});
-return deferred.promise;
+function render(options) {
+  return fs.read('./rss.jade')
+    .then(function(content) {
+      console.log(content);
+      return pug.render(content, options, function(err, xml) {
+        console.log(xml)
+        if (err) {
+          throw err;
+        }
+        return xml;
+      });
+    })
 }
 
 router.get('/rss', function(req, res) {
-  var files = fs.readdirSync(basepath);
-  var posts = preparePosts(files);
-  posts.then(function(result){
-    options.posts = result;
-    return render(options);
-  }).then(function(xml){
-    res.setHeader('content-type', 'application/xml');
-    return res.send(xml);
-  }).done();
+  console.log("rss requested")
+  res.setHeader('content-type', 'application/xml');
+  return res.send(feed);
+
 });
 
- app.use('/', router);
- app.use('/media', express.static(basepath));
- app.listen(program.port);
- console.log('Magic happens on port ' + program.port);
+function getFiles() {
+  return fs.listTree(basepath).then(function(paths) {
+    return paths.filter(function(path) {
+      if (path.split('.').pop() == 'mp3') {
+        return true;
+      }
+    })
+  })
+}
+
+getFiles()
+  .then(function(files) {
+    return preparePosts(files)
+  })
+  .then(function(posts) {
+
+    options.posts = posts;
+    return render(options)
+  })
+  .then(function(xml) {
+    feed = xml;
+  })
+  .then(function() {
+    app.use(compression());
+    app.use('/', router);
+    app.use('/media', express.static(basepath));
+    app.listen(program.port);
+    console.log('Magic happens on port ' + program.port);
+
+  }).catch(function(err) {
+    console.error(err);
+  })
